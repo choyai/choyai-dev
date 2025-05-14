104397cba8737accc12ff8f3e2e877fefb4a1032
choyai <choyaichaiyo@gmail.com>
2025-05-14 09:36:17 +0700

    init

---
+++ web/src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import '../app.css';
+	
+	let { children } = $props();
+</script>
+
+
+<title>choyai</title>
+<div class="bg-slate-900 h-screen w-screen text-cyan-50" >
+{@render children()}
+</div>
---
+++ web/src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script  lang="ts">
+import RollInput from '$lib/component/RollInput.svelte'
+</script> 
+
+<div class="flex items-center h-screen">
+  <div class="m-auto">
+    Enter the dice you want to roll
+  </div>
+    <RollInput/>
+</div>
